be0ba4f6a800fc9f727ded7eea87fbf560df26c0
Ardillán <ardillan@me.com>
2019-09-30 23:08:24 +0200

    Añade configuración de PrimsJS

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,7 +40,16 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
-          `gatsby-remark-prismjs`,
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: "language-",
+              inlineCodeMarker: null,
+              aliases: {},
+              showLineNumbers: false,
+              noInlineHighlight: false,
+            },
+          },
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
         ],
