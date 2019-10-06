473c311604250af4731d01b92fadf30a3103e793
Ardillán <ardillan@me.com>
2019-10-06 12:17:03 +0200

    Muestra mes al principio del listado

--- src/templates/scraper-torrelavega-es.js
+++ src/templates/scraper-torrelavega-es.js
@@ -1,21 +1,45 @@
 import React from "react"
 
 import Layout from "../components/layout"
+import SEO from "../components/seo"
+
 import { formatDate } from "../utils/helpers"
 
 export default ({ pageContext: { data } }) => {
   const scraperData = data.scraperData
+
+  let currentMonth
+
+  const getMonth = date =>
+    new Date(date).toLocaleDateString("es-ES", {
+      month: "long",
+    })
+
+  const showMonth = date => {
+    let nextMonth = getMonth(date)
+    if (currentMonth === nextMonth) {
+      return ""
+    } else {
+      currentMonth = nextMonth
+      return nextMonth
+    }
+  }
   return (
     <Layout>
-      <div className="scraper-page">
+      <SEO
+        title="Scraper de Torrelavega.es"
+        description="Pequeña utilidad que recoge los datos de la web del ayuntamiento de Torrelavega, los muestra y realizar un pequeño análisis."
+      />
+      <div className="scraper-layout blog-layout">
         <section>
           <div className="columns">
             <div className="column is-12">
-              <h1 className="title">Torlavega</h1>
-              <h2 className="subtitle">
-                Proyecto web colaborativo que trata de recopilar el máximo
-                número de datos disponibles para los ciudadanos de Torlavega
-              </h2>
+              <h1 className="title">Datos del ayuntamiento</h1>
+              <p className="subtitle">
+                Esta página recoge y muestra la información que hay en la web
+                del ayuntamiento de Torrelavega, principalmente dentro de su
+                sección noticias.
+              </p>
             </div>
           </div>
         </section>
@@ -23,23 +47,44 @@ export default ({ pageContext: { data } }) => {
         <section>
           <div className="columns">
             <div className="column is-12">
+              <h4>Explicación</h4>
+              <p>
+                Esta pequeña utilidad se lanza principalmente para analizar las
+                últimas 500 noticias que hay en la página web del ayuntamiento
+                de Torrelavega, concretamente de{" "}
+                <a href="http://torrelavega.es/index.php/ciudad/mas-noticias">
+                  esta sección.
+                </a>{" "}
+              </p>
+
+              <h4>Objetivo</h4>
+              <p>
+                El objetivo será recorrer las últimas 500 noticias del
+                ayuntamiento, analizar los datos disponibles y obtener
+                conclusiones acerca de la información obtenida.
+              </p>
+              <hr />
+              <h2>Listado de noticias</h2>
               <ul>
-                {scraperData.map((value, index) => {
+                {scraperData.map((value, index, array) => {
                   return (
-                    <li key={index}>
-                      <a
-                        href={`http://www.torrelavega.es${value.link}`}
-                        target="_blank"
-                        title={value.title}
-                        rel="noopener noreferrer"
-                      >
-                        {value.title}
-                      </a>
-                      <br />
-                      <time dateTime={value.date}>
-                        {formatDate(value.date, "Readable")}
-                      </time>
-                    </li>
+                    <div key={index}>
+                      <h2>{showMonth(value.date)}</h2>
+                      <li>
+                        <a
+                          href={`http://www.torrelavega.es${value.link}`}
+                          target="_blank"
+                          title={value.title}
+                          rel="noopener noreferrer"
+                        >
+                          {value.title}
+                        </a>
+                        <br />
+                        <time dateTime={value.date}>
+                          {formatDate(value.date, "Readable")}
+                        </time>
+                      </li>
+                    </div>
                   )
                 })}
               </ul>
