cd9bf4554d6a7fe0868b35b976a0d211144b5de6
Ardillán <ardillan@me.com>
2019-11-05 13:26:48 +0100

    Añade componente loader

---
+++ src/components/loader.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+const loader = () => {
+  return (
+    <div className="lds-grid">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
+
+export default loader
